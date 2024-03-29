f7060325f001f3db438628aeca43b5ce5ac3b905
devJiraynor <jiraynor@naver.com>
2024-03-29 12:26:45 +0900

    2024.03.29. 12:50

--- es6+/6.destructuring/destructuring.js
+++ es6+/6.destructuring/destructuring.js
@@ -30,7 +30,10 @@ let fruits = ['apple', 'banana', 'melon', 'mango', 'orange'];
     - 할당 기준은 인덱스
 */
 // fruits = ['apple', 'banana', 'melon', 'mango', 'orange'];
-const [fruit1, fruit2, fruit3] = fruits;
+
+// 만약 배열 디스트럭처링에 배열의 길이를 초과한 변수를 선언하면
+// 초과한 범위에 대하여 해당 변수는 undefined 가 됨
+const [fruit1, fruit2, fruit3, fruit4, fruit5, fruit6] = fruits;
 console.log(fruit1);
 console.log(fruit2);
 console.log(fruit3);
@@ -39,28 +42,40 @@ console.log('========================================');
 
 /*
     spread 연산자를 활용
-    - 
+    - 배열 디스트럭처링 마지막에 스프레드 연산자를 사용하면 나머지 모든 요소로 새로운 배열을 생성
 */
-
-
+const [anotherFruit, ...otherFruits] = fruits;
+console.log(anotherFruit);
+console.log(otherFruits);
 
 console.log('========================================');
 
 /*
     객체 디스트럭처링
-    - 
-    - 
+    - 객체의 각 속성을 객체로부터 추출하여 변수에 할당
+    - 할당 기준 키
 */
+// king = {
+//     name: '이성계',
+//     tombName: '태조',
+//     birth: '1335.11.04.',
+//     death: '1408.06.27.'
+// };
 
-
+// 객체 디스트럭처링에서는 변수의 순서는 상관없지만, 이름은 반드시 객체의 키의 이름과 같아야함
+const { tombName, name } = king;
+console.log(name);
+console.log(tombName);
 
 console.log('========================================');
 
 /*
     spread 연산자를 활용
-    - 
+    - 객체 디스트럭처링 마지막에 스프레드 연산자를 사용하면 나머지 모든 속성으로 새로운 객체를 생성
 */
-
-
+const { birth, death, ...reftKing } = king;
+console.log(birth);
+console.log(death);
+console.log(reftKing);
 
 console.log('========================================');
